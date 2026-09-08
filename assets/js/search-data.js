a6a83d0a606fecc75c50b3ac1d76a6fff3adb2e8
kaylairish <43975790+kaylairish@users.noreply.github.com>
2026-09-08 22:17:15 +0000

    Deploying to gh-pages from @ kaylairish/kaylairish.github.io@54672261f5123e6be5a06d9d41f4be3953d6256b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,6 +58,13 @@ ninja.data = [{
           window.open("mailto:%6B%61%79%69%72%69%73%68@%75%77.%65%64%75", "_blank");
         },
       },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/kaylairish", "_blank");
+        },
+      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
